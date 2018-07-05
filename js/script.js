5eefedd8d0c37dad4adfd98eac9c8c5441f6f2ea
Duvan Pedraza <duvansteven14@gmail.com>
2018-07-04 21:30:34 -0500

    actualizacion

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 function inicial(num){
     formulario.resultado.value = formulario.resultado.value + num;
-    var n = [];
-    for(var j = 0; i >= n[]; j++){
-        
-    } 
+    
+    
 }
 
 
-function operacion(x){
-    var a=[];
-    for(var i = 0;i  ;i++){
-
+function operacion(x, res){
+    
+    var numeros = [];
+    var operacion = [];
+    var i = 0;
+    
+    for(var i = 0; i <= numeros.length ; i++){
+        
+        numeros[i] = res;
+        i++;  
+        formulario.resultado.value = "";
+        
     }
+    
+    alert(numeros[0,1]);
+
 }
