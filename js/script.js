17a4162c2639664ea58d10bd5c94932b584bd755
Duvan Pedraza <duvansteven14@gmail.com>
2018-07-04 20:26:24 -0500

    Proyecto calculadora primera version

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function inicial(num){
+    formulario.resultado.value = formulario.resultado.value + num;
+    var n = [];
+    for(var j = 0; i >= n[]; j++){
+        
+    } 
+}
+
+
+function operacion(x){
+    var a=[];
+    for(var i = 0;i  ;i++){
+
+    }
+}
